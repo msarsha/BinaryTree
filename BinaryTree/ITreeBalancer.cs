1f4880ed125c84306ee02b3e3698d4104a5e3f99
Matan <smatan84@gmail.com>
2014-03-10 07:26:29 +0200

    Project Added

---
+++ BinaryTree/ITreeBalancer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public interface ITreeBalancer<T> 
+        where T : IComparable
+    {
+        /// <summary>
+        /// Balance a given node
+        /// </summary>
+        /// <param name="node">node to balance</param>
+        /// <returns>balanced node</returns>
+        void Balance(ref BinaryTreeNode<T> node);
+    }
+}
